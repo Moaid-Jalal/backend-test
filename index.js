b3b5c2593fbf3d0ae725b6ccfb1ae9a24bf56e2e
moaid <moaidjalal2007@gmail.com>
2025-04-20 09:22:44 -0700

    osada

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-const allowedOrigins = ['https://users-ten-beryl.vercel.app', 'https://nsaioabuy-v-aushb.vercel.app', 'http://localhost:3000', 'http://localhost:3000'];
-
+const allowedOrigins = ['https://users-ten-beryl.vercel.app', 'https://nsaioabuy-v-aushb.vercel.app'];
 
 app.use(cors({
   origin: function (origin, callback) {
