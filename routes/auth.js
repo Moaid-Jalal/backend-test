88045e036232159e45fc5000a5400a8060ca18ca
test <  >
2025-07-11 11:34:48 -0700

    masc

--- routes/auth.js
+++ routes/auth.js
@@ -6,49 +6,6 @@ const cookieParser = require('cookie-parser');
 
 const { body, validationResult } = require('express-validator');
 const db = require('../config/database');
-const { auth } = require('../middleware/auth');
-
-// Register new admin
-// router.post('/register',
-//   [
-//     body('email').isEmail().normalizeEmail(),
-//     body('password').isLength({ min: 6 }),
-//     body('name').notEmpty().trim().escape(),
-//   ],
-//   async (req, res) => {
-//     const errors = validationResult(req);
-//     if (!errors.isEmpty()) {
-//       return res.status(400).json({ errors: errors.array() });
-//     }
-
-//     try {
-//       const { name, email, password } = req.body;
-
-//       // Check if user exists
-//       const [existingUser] = await db.query('SELECT * FROM users WHERE email = ?', [email]);
-//       if (existingUser.length > 0) {
-//         return res.status(400).json({ message: 'User already exists' });
-//       }
-
-//       // Hash password
-//       const salt = await bcrypt.genSalt(10);
-//       const hashedPassword = await bcrypt.hash(password, salt);
-
-//       // Create user
-//       const [result] = await db.query(
-//         'INSERT INTO users (name, email, password) VALUES (?, ?, ?)',
-//         [name, email, hashedPassword]
-//       );
-
-//       res.status(201).json({
-//         message: 'User registered successfully',
-//       });
-//     } catch (error) {
-//       console.log(error.message)
-//       res.status(500).json({ message: 'Error registering user', error: error.message });
-//     }
-//   }
-// );
 
 router.use(cookieParser());
 
@@ -125,7 +82,6 @@ router.get('/check', async (req, res) => {
   }
 });
 
-// Logout
 router.post('/logout', (req, res) => {
   res.clearCookie('token', {
     httpOnly: true,
@@ -135,23 +91,5 @@ router.post('/logout', (req, res) => {
 
   res.json({ message: 'Logged out successfully' });
 });
-
-// Get current user (protected route)
-// router.get('/me', auth, async (req, res) => {
-//   try {
-//     const [user] = await db.query(
-//       'SELECT id, name, email, created_at FROM users WHERE id = ?',
-//       [req.user.userId]
-//     );
-
-//     if (user.length === 0) {
-//       return res.status(404).json({ message: 'User not found' });
-//     }
-
-//     res.json(user[0]);
-//   } catch (error) {
-//     res.status(500).json({ message: 'Error fetching user', error: error.message });
-//   }
-// });
 
 module.exports = router;
